81d5f50352d8fb0ad90167b37242bde0ab65cab6
Will Iverson <wiverson@gmail.com>
2023-08-30 22:41:22 -0700

    Use correct JSON lib

--- Assets/App/com/example/UnitySession.cs
+++ Assets/App/com/example/UnitySession.cs
@@ -1,11 +1,23 @@
 using System;
 using System.IO;
+using Newtonsoft.Json;
 using Supabase.Gotrue;
 using Supabase.Gotrue.Interfaces;
-using Unity.Plastic.Newtonsoft.Json;
 using UnityEngine;
 namespace com.example
 {
+	/// <summary>
+	/// This is a simple implementation of IGotrueSessionPersistence that uses Unity's
+	/// Application.persistentDataPath to store the session.
+	///
+	/// This implementation works on iOS, Android, as well as desktop platforms and the Unity Editor.
+	///
+	/// Because this implementation uses the .NET System.IO packages, it will work even if SaveSession is called
+	/// by the refresh background thread.
+	///
+	/// Don't use the Unity PlayerPrefs to store the session - you can only use PlayerPrefs from the main UI thread,
+	/// and this will break the refresh background thread.
+	/// </summary>
 	public class UnitySession : IGotrueSessionPersistence<Session>
 	{
 		private static string FilePath()
@@ -30,8 +42,6 @@ namespace com.example
 				using StreamWriter file = new(filePath);
 				file.Write(str);
 				file.Dispose();
-				// NotificationManager.PostMessage(NotificationManager.NotificationType.Debug,
-				// 	"Session Saved");
 			}
 			catch (Exception e)
 			{
@@ -47,8 +57,6 @@ namespace com.example
 			if (File.Exists(filePath))
 			{
 				File.Delete(filePath);
-				// NotificationManager.PostMessage(NotificationManager.NotificationType.Debug,
-				// 	"Session Deleted");
 			}
 		}
 
